3a4726d66892cdc11cf7e58ba0f1edc4f5749d3d
Monica Sue <monicasue567@gmail.com>
2023-04-01 10:15:02 -0400

    update deal functionality to deal 5 cards to both computer and player

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 /*------------ Constants ------------*/
+const cardValues = {A: 1, 02: 2, 03: 3, 04: 4, 05: 5, 06: 6, 07: 7, 08: 8, 09: 9, 10: 10, J: 10, Q: 10, K: 10}
 
 
 /*------------ Variables ------------*/
@@ -10,13 +11,16 @@ let playerDiscard = []
 let cardToRemove, turn, winner // understand this concept from lecture
 
 
+
 /*---- Cached Element References ----*/
 //card elements
 const deckEl = document.getElementById('deck')
-const computerHandEl = document.getElementById('computer-hand')
+const computerHandContainerEl = document.getElementById('computer-hand-container')
 const computerDiscardEl = document.getElementById('computer-discard')
+
 const playerHandContainerEl = document.getElementById('player-hand-container')
 const playerDiscardEl = document.getElementById('player-discard')
+
 
 // test deck elements - worked!
 // console.log(deckEl, 'deck')
@@ -25,15 +29,20 @@ const playerDiscardEl = document.getElementById('player-discard')
 // console.log(playerHandEl, 'player hand')
 // console.log(playerDiscardEl, 'player discard')
 
+
 //button elements
 const dealBtnEl = document.getElementById('deal')
 const discardBtnEl = document.getElementById('discard')
 const compareBtnEl = document.getElementById('compare-hands')
+const pickUpBtnEl = document.getElementById('pick-up-card')
+
+
 const resetBtnEl = document.getElementById('reset')
 
 //test button elements - worked!
 // console.log(compareBtnEl, 'compare btn')
 // console.log(resetBtnEl, 'reset btn')
+// console.log(pickUpBtnEl, 'pick-up-card')
 
 //message element
 const messageEl = document.getElementById("message")
@@ -46,58 +55,129 @@ const messageEl = document.getElementById("message")
 dealBtnEl.addEventListener('click', handleDeal)
 discardBtnEl.addEventListener('click', ()=> console.log('clicked', 'discard'))
 compareBtnEl.addEventListener('click', ()=> console.log('clicked', 'compare'))
+
+//player event listener
+document.addEventListener('click', handleSelection) //{
+//   playerHand.forEach((num, idx) => {
+//   const target = evt.target.closest(`#P${idx}`)
+//   console.log(target)
+//   })
+// })
+
+
 resetBtnEl.addEventListener('click', init)
 
 /*------------ Functions ------------*/
 
 init()
 
-// Initialize deck 1 with array of 52 cards 
+// Initialize deck with array of 52 cards 
 function init(){
   deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+  compareBtnEl.disabled = true
+  computerHand = []
+  playerHand = []
+  turn = 1
+  
 }
 
+//// Deal random hand (5 cards) to each player
+// Deal button clicked THEN remove card from deck and pushes into player hand array + disables Deal button after
 function handleDeal(){
   if (deck.length > 0) {
     for (let index = 0; index < 5; index++) {
       let randIdx = Math.floor(Math.random() * deck.length)
       let playerDealtCard = deck.splice(randIdx, 1)[0]
       playerHand.push(playerDealtCard)
-      render(playerDealtCard)
-      dealBtnEl.disabled = true
+      renderPlayer(playerDealtCard)
+      }
     }
-
-      
-    }
-    
-    
-    // deal code works but only one at a time
-    // let randIdx = Math.floor(Math.random() * deck.length)
-    // let playerDealtCard = deck.splice(randIdx, 1)[0]
-    // playerHand.push(playerDealtCard)
-    // render(playerDealtCard)
-  //}
+  if (deck.length > 0) {
+    for (let index = 0; index < 5; index++) {
+      let randIdx = Math.floor(Math.random() * deck.length)
+      let computerDealtCard = deck.splice(randIdx, 1)[0]
+      computerHand.push(computerDealtCard)
+      renderComputer(computerDealtCard)
+      }
+    } 
+  dealBtnEl.disabled = true
 }
 
-function render() {
-  playerHandContainerEl.innerHTML = ''
-  playerHand.forEach((card, idx) => {
-    appendPlayerCard(card, idx)
-  })
-}
-
-
-
+// Appends the player cards to the player hand container
 function appendPlayerCard(playerDealtCard, idx) {
-  console.log('appending', playerDealtCard)
   let playerCard = document.createElement('div')
   playerCard.className = 'card large'
   playerCard.classList.add(playerDealtCard)
+  playerCard.id = `P${idx}`
   playerHandContainerEl.appendChild(playerCard)
-  }
+}
 
-// Deal random hand (5 cards) to each player
+function appendComputerCard(computerDealtCard, idx) {
+  let computerCard = document.createElement('div')
+  computerCard.className = 'card large'
+  computerCard.classList.add(computerDealtCard)
+  computerCard.id = `C${idx}`
+  computerHandContainerEl.appendChild(computerCard)
+}
+
+function renderPlayer() {
+  playerHandContainerEl.innerHTML = '' // clean state
+  playerHand.forEach((playerDealtCard, idx) => {
+    appendPlayerCard(playerDealtCard, idx)
+  })
+}
+
+function renderComputer() {
+  computerHandContainerEl.innerHTML = '' // clean state
+  computerHand.forEach((computerDealtCard, idx) => {
+    appendComputerCard(computerDealtCard, idx)
+  })
+}
+// Above code for 5 hand deal -- beginning of each hand`
+
+//selecting & highlighting cards to discard
+function handleSelection(evt) {
+  playerHand.forEach((num, idx) => {
+    const target = evt.target.closest(`#P${idx}`)
+    console.log(target)
+  })
+} 
+
+
+//switch player function
+// function switchPlayerTurn() {
+//   if (winner === true) {
+//     return
+//   } else {
+//     turn *= -1
+//   }
+
+// }
+
 // If player hand total is less than or equal to 5, compare hands button becomes available & player may ‘click button’:
+// enabling compare button
+// function compareEnable() {
+//   let playerValue = 0
+//   playerHand.forEach(card => {
+//     console.log(parseInt(card.slice(1)))
+//     playerValue += parseInt(card.slice(1))
+//     console.log(playerValue)
+//   });
+
+// }
+
+
+
+
+// Else player will select the card(s) to remove from their hand and ‘click the discard button' 
+// If multiple cards are discarded, the player unlocks "license" 
+// Player will pick up one card from the communal deck or if the player obtained "license", they can select 1 card from the opponent's last turn discard pile and place it in their hand
+// Next player's turn, return to step 2
+
+
+
+
+//scoring
 // If the player's hand total is less than the opponent's hand value, the player wins!
 // Player will have a score value of 0 in this round to be added to total game score
 // Opponent will have a score value of total hand value to be added to total game score 
@@ -108,7 +188,3 @@ function appendPlayerCard(playerDealtCard, idx) {
 // If player's game score is true, game is over & the opponent wins, or vice-versa
 // Reset game functionality
 // Else new round is Rendered, return to step 1
-// Else player will select the card(s) to remove from their hand and ‘click the discard button' 
-// If multiple cards are discarded, the player unlocks "license" 
-// Player will pick up one card from the communal deck or if the player obtained "license", they can select 1 card from the opponent's last turn discard pile and place it in their hand
-// Next player's turn, return to step 2
